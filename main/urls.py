17e288f6b5ea7b5e4bf106a368968f29a0a4dafa
zere <zerekudarova@gmail.com>
2026-09-22 17:43:34 +0500

    change app_detail, about and app_list into class type views. Create cheap and expensive apps

--- main/urls.py
+++ main/urls.py
@@ -3,16 +3,21 @@ from django.urls import path, include, URLPattern
 from . import views
 
 
+
 app_name = 'main'
 
 urlpatterns=[
-    path('', views.index, name='index'),
+    path('', views.AppListView.as_view(), name='index'),
     path('hw/', views.hw, name='hw'),
-    path('about/', views.about, name='about'),
+    path('about/',views.AboutView.as_view(), name='about'),
     path('reviews/', views.reviews, name='reviews'),
     path('category/<int:category_id>/', views.category_detail, name='category'),
-    path('app_detail/<int:app_id>/<str:app_name>/', views.app_detail, name='app_detail'),
+    path('app_detail/<int:app_id>/<str:app_name>/', views.AppDetailView.as_view(), name='app_detail'),
     path('free/', views.free, name='free'),
     path('new/', views.new, name='new'),
+    path('api/review_detail/<int:review_id>/', views.review_detail, name='review_detail'),
+    path('cheap/', views.cheap, name='cheap'),
+    path('cheap/', views.cheap_apps, {'max_price':500}, name='cheap_apps'),
+    path('premium/', views.cheap_apps, {'min_price': 5000}, name='premium_apps'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,47 +1,80 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 from main.models import App, Category, Review
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.views.generic import TemplateView, ListView, DetailView
 
+
+
+
+@require_GET
 def hw(request):
-    return HttpResponse("This is my home work")
+    return HttpResponse("This is my homework")
+class AppListView(ListView):
+    model = App
+    template_name = 'main/index.html'
+    paginate_by =4
+    context_object_name = 'apps'
 
-def index(request):
-    apps = App.objects.all()
-    categories = Category.objects.all()
-    q = request.GET.get('q', '')
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        sort=self.request.GET.get('sort', 'new')
+
+        if q:
+            apps = App.objects.filter(Q(name__icontains=q) | Q(description__icontains=q)).order_by('name')
+
+        else:
+            apps = App.objects.order_by('-created_at')
+
+        return apps
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
 
 
-    if q:
-        apps = App.objects.filter(Q(name__icontains=q) | Q(description__icontains=q)).order_by('name')
+class AppDetailView(DetailView):
+    model = App
+    template_name = 'main/app_detail.html'
+    context_object_name = 'app'
+    pk_url_kwarg = 'app_id'
 
-    else:
-        apps = App.objects.order_by('-created_at')
+    def get_object(self):
+        app=super().get_object()
+        if app.name!=self.kwargs['app_name']:
+            return redirect('main:app_detail', app.id, app.name)
+        return app
 
-    paginator = Paginator(apps, 4)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
 
-    return render(request, 'main/index.html', {
-        'page_obj': page_obj,
-        'categories': categories,
-        'q': q,
+    data={
+        'review_id': review.id,
+        'app': review.app.name,
+        'username': review.username,
+        'comment': review.comment,
+        'stars': review.stars,
+        'recommended': review.recommended,
+        'created_at': review.created_at,
+    }
 
-    })
+    return JsonResponse(data)
 
-def about(request):
-    apps=App.objects.all()
-    return render(request, 'main/about.html')
+
+
+
+
 
 def reviews(request):
     reviews=Review.objects.all()
     return render(request, 'main/reviews.html', {'reviews': reviews})
 
 def category_detail(request, category_id):
-
+    categories = Category.objects.all()
     category = get_object_or_404(Category, id=category_id)
     apps = App.objects.filter(category=category)
     q = request.GET.get('q', '')
@@ -59,24 +92,26 @@ def category_detail(request, category_id):
 
     return render(request, 'main/category_detail.html', {
         'category': category,
+        'categories': categories,
         'apps': apps,
         'page_obj': page_obj,
         'q': q,
     })
+#
+# def app_detail(request, app_id, app_name):
+#     app = get_object_or_404(App, id=app_id)
+#
+#     if app_name != app.name:
+#         return redirect('main:app_detail', app.id, app.name)
+#
+#
+#     print(app_name)
+#
+#     return render(request, 'main/app_detail.html', {
+#         'app': app,
+#
+#     })
 
-def app_detail(request, app_id, app_name):
-    app = get_object_or_404(App, id=app_id)
-
-    if app_name != app.name:
-        return redirect('main:app_detail', app.id, app.name)
-
-
-    print(app_name)
-
-    return render(request, 'main/app_detail.html', {
-        'app': app,
-
-    })
 
 
 
@@ -100,3 +135,24 @@ def new(request):
         'page_obj': page_obj,
 
     })
+
+def cheap(request):
+    apps = App.objects.filter(price__lte=500)
+    paginator = Paginator(apps, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'main/cheap.html', {'page_obj': page_obj})
+
+def cheap_apps(request, min_price=None, max_price=None):
+    if min_price is not None:
+        apps = App.objects.filter(price__gte=500)
+    elif max_price is not None:
+        apps = App.objects.filter(price__lte=5000)
+
+    paginator = Paginator(apps, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(request, 'main/cheap.html', {'page_obj': page_obj})
+
